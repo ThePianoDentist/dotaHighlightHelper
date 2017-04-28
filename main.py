5d4345fdffb5cf1b4a940bb78f6b3d34c3ee82d6
ThePianoDentist <jbknight07@gmail.com>
2017-04-28 03:19:56 +0100

    Added new searches. commit before move to using odota explorer files for match generation

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ import json
 import time
 
 from constants import *
-from search import SmokeSearch
+from search import SmokeSearch, MultikillSearch
 
 
 def command_line_options():
     parser = argparse.ArgumentParser(description='Bulk download dota replays, plus analysis')
-    parser.add_argument('team_id', type=int, help='TeamID of team to analyse')
-    parser.add_argument('--patch', '-p', type=str, dest="patch", help='DotA patch number. I.e. 7.05')
+    parser.add_argument('-odota', '-o', dest='odota_file', type=str, help="File-path of open-dota explorer json results")
+    parser.add_argument('--team', '-t', dest='team_id', type=int, help='TeamID of team to analyse')
+    parser.add_argument('--patch', '-P', type=str, dest="patch", help='DotA patch number. I.e. 7.05')
     # TODO replace default of smoke timings when have others
+    parser.add_argument('--player', '-p', type=str, dest="player", help='Player account ID')
+    parser.add_argument('--hero', '-h', type=str, dest="hero_id", help='Hero ID')
+    parser.add_argument('--start', '-s', type=str, dest="start_time", help='Start time (yyyy-mm-dd)')
+    parser.add_argument('--end', '-e', type=str, dest="end_time", help='End time (yyyy-mm-dd)')
+    parser.add_argument('--league', '-l', type=str, dest="league_id", help='Player account ID')
+
     parser.add_argument('--search', '-s', default="smoketimings", type=str, dest="search_type",
                         help='Search for this in replays. I.e. teamfights')
     parser.add_argument('--tickOffset', default=2, type=float, dest="tickOffset", help="Seconds before event to log")
@@ -20,10 +27,13 @@ def command_line_options():
                         action='store_true')
     parser.add_argument('--file', '-f', type=str, dest="out_file", help='File to save scripts results to')  # TODO check
     args = parser.parse_args()
-    return args.team_id, args.patch, args.search_type, args.out_file, args.onlyDownload, args.list, args.tickOffset
+    return args.odota_file, args.team_id, args.patch, args.search_type, args.out_file, args.onlyDownload, args.list,\
+           args.tickOffset
 
 
-def validate_input(list_searches, search_type, only_download):
+def validate_input(odota_file, list_searches, search_type, only_download):
+    if odota_file:
+        return
     if list_searches:
         print("Supported searches:\n")
         print("\n".join(ALLOWABLE_SEARCHES))
@@ -43,26 +53,27 @@ def main():
     with open("hero_ids.json") as f:  # TODO use constants package?
         heroes_dict = json.load(f)
 
-    team_id, patch, search_type, out_file, only_download, list_searches, tick_offset = command_line_options()
+    odota_file, team_id, patch, search_type, out_file, only_download, list_searches, tick_offset = command_line_options()
     # args
     #team_id = 2108395 # TNC
-    validate_input(list_searches, search_type, only_download)
+    validate_input(odota_file, list_searches, search_type, only_download)
 
     print("Searching for team: %d" % team_id)
     #search = make_search(search_type)
-    search = SmokeSearch(team_id, only_download, patch=patch, tick_offset=tick_offset)
-
-    search.find_match_ids()
-    search.add_matches()
-    search.download_replays()
+    search = SmokeSearch(team_id, only_download, patch=patch, tick_offset=tick_offset, odota_file=odota_file)
+    search2 = MultikillSearch(team_id, only_download, patch=patch, tick_offset=tick_offset, odota_file=odota_file)
+    search2.find_match_ids_odota()
+    # search2.find_match_ids()
+    # search2.add_matches()
+    search2.download_replays()
 
     if out_file is not None:
         with open(out_file, "w") as f:
-            for x in search.iterate_matches(heroes_dict):
+            for x in search2.iterate_matches(heroes_dict):
                 f.write(x)
     else:
         start = time.time()
-        for x in search.iterate_matches(heroes_dict):
+        for x in search2.iterate_matches(heroes_dict):
             print(x, end='')
         print("Time taken: %f" % (time.time() - start))
 
--- search.py
+++ search.py
@@ -12,7 +12,15 @@ from match import Match
 
 class Search(object):
 
-    def __init__(self, team_id, only_download, patch=None, time_range=None, tick_offset=60):
+    def __init__(self, team_id, only_download, patch=None, time_range=None, tick_offset=60, odota_file=None):
+        """
+
+        :param team_id:
+        :param only_download:
+        :param patch:
+        :param time_range:
+        :param tick_offset:
+        """
         # TODO what is reasonable timerange
         self.matches_by_id = {}
         self.search_phrase = None
@@ -23,6 +31,9 @@ class Search(object):
         self.time_range = time_range
         self.only_download = only_download
         self.tick_offset = tick_offset
+        self.multiline = False
+        self.odota_file = odota_file
+        self.hero = None
 
     def match_from_id(self, match_id):
         """
@@ -30,44 +41,65 @@ class Search(object):
         """
         return self.matches_by_id[match_id]
 
-    def find_match_ids(self):
-        # Hard to tidy as its just one query param (sql)
-        url = "https://api.opendota.com/api/explorer?sql=SELECT%0Amatches.match_id%2C%0Amatches.start_time%2C%0A((player_matches.player_slot%20%3C%20128)%20%3D%20matches.radiant_win)%20win%2C%0Aplayer_matches.hero_id%2C%0Aplayer_matches.account_id%2C%0Aleagues.name%20leaguename%0AFROM%20matches%0AJOIN%20match_patch%20using(match_id)%0AJOIN%20leagues%20using(leagueid)%0AJOIN%20player_matches%20using(match_id)%0ALEFT%20JOIN%20notable_players%20using(account_id)%0ALEFT%20JOIN%20teams%20using(team_id)%0AJOIN%20heroes%20ON%20player_matches.hero_id%20%3D%20heroes.id%0AWHERE%20TRUE%0AAND%20notable_players.team_id%20%3D%20{0}".format(self.team_id)
-        if self.patch:
-            url += "%0AAND%20match_patch.patch%20%3D%20%27{0}%27".format(self.patch)
-        url += "%0AORDER%20BY%20matches.match_id%20DESC%20NULLS%20LAST%0ALIMIT%20200"
-        match_ids_resp = request_(url)
-        self.match_ids = set(x["match_id"] for x in json.loads(match_ids_resp)["rows"])
+    def find_match_ids_odota(self):
+        with open(self.odota_file) as f:
+            data = json.load(f)
+        for match in data["rows"]:
+            if match["match_id"] not in self.match_ids:
+                self.match_ids.add(match["match_id"])
+                self.matches_by_id[match.id] = match
+            else:
+                match.add_hero(match["hero_id"])
 
-    def add_matches(self):
-        """
-        We need to store what heros are on our team, to allow for easier replay parsing/choosing what ticks apply to us
-        :return:
-        """
-        for match_id in self.match_ids:
-            match = Match(match_id)
-            if not self.only_download:
-                # No sleep time because the Match() object sleeps for 1 second for its own API requests
-                match_details = json.loads(get_match_details(match_id, APIKEY, sleep_time=0))["result"]
-                if match_details["radiant_team_id"] == self.team_id:
-                    match.add_side(True)
-                elif match_details["dire_team_id"] == self.team_id:
-                    match.add_side(False)
-                else:
-                    raise Exception("Team is not in this match")
+    # def find_match_ids(self):
+    #     # Hard to tidy as its just one query param (sql)
+    #     url = "https://api.opendota.com/api/explorer?sql=SELECT%0Amatches.match_id%2C%0Amatches.start_time%2C%0A((player_matches.player_slot%20%3C%20128)%20%3D%20matches.radiant_win)%20win%2C%0Aplayer_matches.hero_id%2C%0Aplayer_matches.account_id%2C%0Aleagues.name%20leaguename%0AFROM%20matches%0AJOIN%20match_patch%20using(match_id)%0AJOIN%20leagues%20using(leagueid)%0AJOIN%20player_matches%20using(match_id)%0ALEFT%20JOIN%20notable_players%20using(account_id)%0ALEFT%20JOIN%20teams%20using(team_id)%0AJOIN%20heroes%20ON%20player_matches.hero_id%20%3D%20heroes.id%0AWHERE%20TRUE%0AAND%20notable_players.team_id%20%3D%20{0}".format(self.team_id)
+    #     if self.patch:
+    #         url += "%0AAND%20match_patch.patch%20%3D%20%27{0}%27".format(self.patch)
+    #     if self.player_id:
+    #         url += "%0AAND%20player_matches.account_id%20%3D%20%27{0}%27".format(self.player_id)
+    #     if self.hero_id:
+    #         url += "%0AAND%20player_matches.hero_id%20%3D%20%27{0}%27".format(self.hero_id)
+    #     if self.start_time:
+    #         url += "%0AAND%20matches.start_time%20%3D%20%27{0}%27".format(self.start_time)
+    #     if self.end_time:
+    #         url += "%0AAND%20matches.start_time%20%3D%20%27{0}%27".format(self.end_time)
+    #     if self.league_id:
+    #         url += "%0AAND%20matches.le agueid%20%3D%20%27{0}%27".format(self.league_id)
+    #     url += "%0AORDER%20BY%20matches.match_id%20DESC%20NULLS%20LAST%0ALIMIT%20200"
+    #     match_ids_resp = request_(url)
+    #     self.match_ids = set(x["match_id"] for x in json.loads(match_ids_resp)["rows"])
 
-                for pick in match_details["picks_bans"]:
-                    if not pick["is_pick"]:
-                        continue
-                    else:
-                        # Dont think I can use fancy bit operators as one is True/False. One 0/1
-                        # if match_id == 3117431107:
-                        #     import pdb; pdb.set_trace()
-                        radiant_pick = match_details["radiant_team_complete"] == pick["team"]  # pick["team"] seems to be slot. not side
-                        if (match.is_radiant and radiant_pick) or (not radiant_pick and not match.is_radiant):
-                            match.add_hero(pick["hero_id"])
-            self.matches.add(match)
-            self.matches_by_id[match.id] = match
+    # def add_matches(self):
+    #     """
+    #     We need to store what heros are on our team, to allow for easier replay parsing/choosing what ticks apply to us
+    #     :return:
+    #     """
+    #
+    #     for match_id in self.match_ids:
+    #         match = Match(match_id)
+    #         if not self.only_download:
+    #             # No sleep time because the Match() object sleeps for 1 second for its own API requests
+    #             match_details = json.loads(get_match_details(match_id, APIKEY, sleep_time=0))["result"]
+    #             if match_details["radiant_team_id"] == self.team_id:
+    #                 match.add_side(True)
+    #             elif match_details["dire_team_id"] == self.team_id:
+    #                 match.add_side(False)
+    #             else:
+    #                 raise Exception("Team is not in this match")
+    #
+    #             for pick in match_details["picks_bans"]:
+    #                 if not pick["is_pick"]:
+    #                     continue
+    #                 else:
+    #                     # Dont think I can use fancy bit operators as one is True/False. One 0/1
+    #                     # if match_id == 3117431107:
+    #                     #     import pdb; pdb.set_trace()
+    #                     radiant_pick = match_details["radiant_team_complete"] == pick["team"]  # pick["team"] seems to be slot. not side
+    #                     if (match.is_radiant and radiant_pick) or (not radiant_pick and not match.is_radiant):
+    #                         match.add_hero(pick["hero_id"])
+    #         self.matches.add(match)
+    #         self.matches_by_id[match.id] = match
 
     def download_replays(self):
         """
@@ -115,7 +147,7 @@ class Search(object):
             match_id_regex = re.compile(r"^\((\d+)\)")
 
             for line in contents:
-                match_id_maybe = match_id_regex.findall(line)
+                match_id_maybe = match_id_regex.findall(contents) if self.multiline else match_id_regex.findall(line)
                 if not match_id_maybe:
                     continue
 
@@ -162,6 +194,74 @@ class SmokeSearch(Search):
             try:
                 hero_id = heroes_dict[hero]["id"]
             except KeyError:  # dumb illusion or some bullshit. we'll catch a real hero on further loops
+                return
+            if hero_id in match.our_heroes:
+                match.interesting_ticks.add(tick)
+
+
+class CourierKillSearch(Search):
+
+    def __init__(self, team_id, only_download, patch=None, time_range=None, tick_offset=60):
+        super().__init__(team_id, only_download, patch, time_range, tick_offset)
+        self.search_phrase = "courierkills"
+        self.search_phrase = "smoketimings"
+
+    def find_interesting_ticks(self, line, match, heroes_dict):
+        """
+
+        :param line: String. log line from java parser
+        :param match: Match
+        :param heroes_dict:
+        :return:
+        """
+        courier_kills = re.search("\[(\d+)\] npc_dota_courier is killed by ([^\s]+)", line)
+        if courier_kills:
+            tick, hero = courier_kills.groups()
+            tick = int(tick)
+            tick -= self.tick_offset  # Allow to move to few seconds before the action
+            try:
+                hero_id = heroes_dict[hero]["id"]
+            except KeyError:  # illusion/tower/`something else` kills courier
+                return
+            if hero_id in match.our_heroes:
+                match.interesting_ticks.add(tick)
+
+
+class MultikillSearch(Search):
+
+    def __init__(self, team_id, only_download, patch=None, time_range=None, tick_offset=60, num_kills=2, odota_file=None):
+        super().__init__(team_id, only_download, patch, time_range, tick_offset, odota_file)
+        self.search_phrase = "multikills"
+        self.search_phrase = "smoketimings"
+        self.num_kills = num_kills
+        assert self.num_kills < 9
+        #self.multiline = True
+
+    def find_interesting_ticks(self, line, match, heroes_dict):
+        """
+
+        :param line: String. log line from java parser
+        :param match: Match
+        :param heroes_dict:
+        :return:
+        """
+
+        # Ignore pycharm error, doesnt understand the string replacement
+        multi_kills = re.search(
+            "\[(\d+)\] ([^\s]+) performs multikill of %d" % self.num_kills,
+            line
+        )
+        # multi_kills = re.search(
+        #     "(?s)type: DOTA_COMBATLOG_MULTIKILL.*?value: (%d).*?\[(\d+)\] [^\s]+ is killed by ([^\s]+)" % self.num_kills,
+        #     line
+        # )
+        if multi_kills:
+            tick, hero = multi_kills.groups()
+            tick = int(tick)
+            tick -= self.tick_offset  # Allow to move to few seconds before the action
+            try:
+                hero_id = heroes_dict[hero]["id"]
+            except KeyError:  # illusion/tower/`something else` kills courier
                 return
             if hero_id in match.our_heroes:
                 match.interesting_ticks.add(tick)
