7e5b7dfe7f6f6ba30593120fddf70d2dde1d7e94
ThePianoDentist <jbknight07@gmail.com>
2017-04-28 19:44:13 +0100

    Remove unneeded stuff

--- api_requests.py
+++ api_requests.py
@@ -31,41 +31,10 @@ def download_and_extract(match):
     # p = subprocess.Popen(["powershell", "-Command", '"Invoke-WebRequest %s -OutFile \'%s\'"' % (req_url, file_path)],
     #                      stdin=subprocess.PIPE, stdout=subprocess.PIPE,
     #                      stderr=subprocess.PIPE)
-    # output, err = p.communicate()
-    # TODO add other platform version. way to do without 7zip dep on windows as well?
     time.sleep(0.1)
     with open(match.file_path, 'wb') as newf, bz2.BZ2File(match.download_path) as oldf:
         for data in iter(lambda : oldf.read(100 * 1024), b''):
             newf.write(data)
-    # p = subprocess.Popen([SEVENZIP_PATH, "x", match.download_path, "-o%s" % folder], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-    #                      stderr=subprocess.PIPE, shell=True) #TODO check not security risk shell true (it really didnt want to do 7zip stuff without it on)
-    # output, err = p.communicate()
-    # print(err)  # alright windows. so it gets "ERROR: Unexpected end of data"...but thats out not err.
-    # print(output)
+
     print("Match successfully downloaded: %d" % match.id)
     os.remove(match.download_path)  # Delete the old compressed replay file
-
-def download_(match):
-    print(match.id)
-    print(match.already_have_replay)
-    if match.already_have_replay:
-        return
-
-    urlretrieve(match.replay_url, match.download_path)
-    print("Match successfully downloaded: %d" % match.id)
-
-
-def extract_(match):
-    if match.already_have_replay:
-        return
-    with open(match.file_path, 'wb') as newf, bz2.BZ2File(match.download_path) as oldf:
-        for data in iter(lambda : oldf.read(100 * 1024), b''):
-            newf.write(data)
-    print("Match successfully extracted: %d" % match.id)
-    os.remove(match.download_path)  # Delete the old compressed replay file
-
-
-def get_match_details(match_id, APIKEY, sleep_time=None):
-    return request_(
-        "http://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/v0001?" \
-        "key=%s&match_id=%s" % (APIKEY, match_id), sleep_time=sleep_time)
--- constants.py
+++ constants.py
@@ -6,11 +6,8 @@ config.read('config.ini')
 
 REPLAY_FOLDER = os.path.normpath(config.get('settings', 'REPLAY_FOLDER'))
 CLARITY_FOLDER = os.path.normpath(config.get('settings', 'CLARITY_FOLDER'))
-APIKEY = os.path.normpath(config.get('settings', 'APIKEY'))
-MAX_PARALLEL_DOWNLOADS = int(config.get('settings', 'MAX_PARALLEL_DOWNLOADS'))
-SEVENZIP_PATH = os.path.normpath(config.get('settings', 'SEVENZIP_PATH'))
 
-ALLOWABLE_SEARCHES = {"smoketimings"}
+ALLOWABLE_SEARCHES = {"smoketimings", "courierkills", "multikills", "teamfights"}
 # Ideas for adding allowable searches
 # Teamfight starts
 # Mid-lane ganks
